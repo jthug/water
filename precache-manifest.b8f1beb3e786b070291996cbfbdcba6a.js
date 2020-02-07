860ef6a39871911a296cd350c59ebb70a4db0528
kou <1649369473@qq.com>
2020-02-07 12:17:41 +0800

    实验

---
+++ precache-manifest.b8f1beb3e786b070291996cbfbdcba6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e9042677987830e8da5fb63fcefa7a",
+    "url": "/water/index.html"
+  },
+  {
+    "revision": "9632399c52731ee8e58c",
+    "url": "/water/static/css/main.dd40a98f.chunk.css"
+  },
+  {
+    "revision": "57d811b8be036c4e2662",
+    "url": "/water/static/js/2.9c7e2929.chunk.js"
+  },
+  {
+    "revision": "99c5655d2432b8199a89e031b7b8d3f7",
+    "url": "/water/static/js/2.9c7e2929.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9632399c52731ee8e58c",
+    "url": "/water/static/js/main.d9e5da77.chunk.js"
+  },
+  {
+    "revision": "b22cfcc829d3c177dbd7",
+    "url": "/water/static/js/runtime-main.7ae0ba9d.js"
+  }
+]);
